b09c436030dc9a8d8f4545aa398097b7bd171663
aleksarad <alexa.radevich1@gmail.com>
2020-06-10 19:05:49 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.create(name: "Nicki Minaj", bio: "Queen of Rap")
+Artist.create(name: "Mariah Carey", bio: "The Elusive Chanteuse")
+Artist.create(name: "Travis Scott", bio: "Cactus Jack")
+
+Genre.create(name:"Pop")
+Genre.create(name:"Rap")
+
+Song.create(name:"Superbass", artist_id: 1, genre_id: 1)
+Song.create(name:"Barbie Tingz", artist_id: 1, genre_id: 2)
+Song.create(name:"We Belong Together", artist_id: 2, genre_id: 1)
+Song.create(name:"Emotions", artist_id: 2, genre_id: 1)
+Song.create(name:"Antidote", artist_id: 3, genre_id: 2)
+Song.create(name:"Sicko Mode", artist_id: 3, genre_id: 2)
+
+
