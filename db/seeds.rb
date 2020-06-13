ea2ba78042a4e0b3a378abd286e86daaae80fef0
j3ffh95 <j3ffh95@gmail.com>
2020-06-12 21:24:27 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "deleting seeds"
+Artist.destroy_all
+Genre.destroy_all
+Song.destroy_all
+
+
+Artist.create(name: "Britney Spears", bio: "Im back!")
+Artist.create(name: "Brunmo mars", bio: "Is uptown funk")
+
+Genre.create(name: "pop")
+Genre.create(name: "jazz")
+
+Song.create(name: "Just Dance", artist_id: 1, genre_id: 2)
+Song.create(name: "Uptown Funk", artist_id: 2, genre_id: 1)
+
+puts "seeding completed"
+# 5.times do
+#   Artist.create(name: Faker::Superhero.name, bio: Faker::Superhero.descriptor)
+# end
+
+# 5.times do
+
+# end
